1f24c9dd3a360a722728d6924d59dbb061a83603
Otto Borchert <borchert-o@mssu.edu>
2019-11-07 11:02:04 -0600

    Initial Commit - only the Quit button works

---
+++ SeventhGUI/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Windows;
+
+namespace SeventhGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Quit_Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
